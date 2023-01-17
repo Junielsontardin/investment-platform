54a6553cede6f30deff23494fecb3b0b5e0c8b2a
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2023-01-17 20:21:29 -0300

    feat: initial project, config webpack, babel, eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './styles.css';
+import LOGO from './assets/logo-react.png';
+import ICON from './assets/icon-watch.svg';
+import { Counter } from './Counter';
+
+const App = () => {
+  const name = {
+    teste: "I'm",
+  };
+
+  const { teste } = name;
+
+  console.log(teste);
+
+  return (
+    <div>
+      <img
+        src={LOGO}
+        alt="Logo"
+        width={300}
+        height={300}
+      />
+      <img
+        src={ICON}
+        alt="Icon"
+      />
+      <h1>
+        Hello World!! {process.env.NODE_ENV} - {process.env.name}
+      </h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
