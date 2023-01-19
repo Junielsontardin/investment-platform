dc1548247cf01ee9dd3ca40376281c46c283b5f0
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2023-01-18 22:45:29 -0300

    feat: adding nested forEach to test sonarCloud analysis

--- src/components/Teste/index.tsx
+++ src/components/Teste/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
+const arrayNormal = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
 const Teste = () => {
+  arrayNormal.forEach((number) => {
+    arrayNormal.forEach((string) => {
+      arrayNormal.forEach((str) => {
+        if (number === string && string === str) {
+          console.log(number);
+        }
+      });
+    });
+  });
+
   return (
     <div className={styles.teste}>
       <select>
