ed1bc3114ad4be0f5757d92eba9b7db53b570272
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2023-01-19 21:15:20 -0300

    feat: adding more complexity to see errors on sonar

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -14,25 +14,41 @@ export const Counter = () => {
     console.log('LELELA');
   }
 
-  const arrayNormal = ['1', '2', '3', '4', '5', '6', '7', '8'];
-  arrayNormal.forEach((number) => {
-    arrayNormal.forEach((string) => {
-      arrayNormal.forEach((str) => {
-        if (number === string && string === str) {
-          console.log(number);
-        }
+  const TestingSonar = () => {
+    const arrayNormal = ['1', '2', '3', '4', '5', '6', '7', '8'];
+    arrayNormal.forEach((number) => {
+      arrayNormal.forEach((string) => {
+        arrayNormal.forEach((str) => {
+          arrayNormal.forEach((teste) => {
+            arrayNormal.forEach((testeTwo) => {
+              arrayNormal.forEach((tete7) => {
+                if (number === string && string === str) {
+                  console.log(number);
+                }
 
-        while (true) {
-          console.log('ALALA');
-        }
+                if (number === teste && string === teste) {
+                  console.log(number);
+                }
+
+                if (tete7 === tete7 && testeTwo === str) {
+                  console.log(number);
+                }
+                while (true) {
+                  console.log('ALALA');
+                }
+              });
+            });
+          });
+        });
       });
     });
-  });
+  };
 
   return (
     <div>
       <h3>Update the count and edit src/App.tsx, state is preserved</h3>
       <button onClick={teste}>Count - {count}</button>
+      <button onClick={TestingSonar}>Clique aqui</button>
     </div>
   );
 };
